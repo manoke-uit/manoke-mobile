65f077f9674bd91059320924f9e5ad29abf1e0a4
hathuanihi <hathu20905@gmail.com>
2025-04-28 14:16:02 +0700

    more menu n karaoke page

---
+++ src/components/moreMenu.tsx
@@ -0,0 +1,171 @@
+import React from "react";
+import { Modal, Pressable, Text, TouchableOpacity, View, StyleProp, ViewStyle } from "react-native";
+import { APP_COLOR } from "@/utils/constant";
+import { Entypo, MaterialIcons, Ionicons } from "@expo/vector-icons";
+
+interface MoreMenuProps {
+  visible: boolean;
+  onClose: () => void;
+  onAddToQueue: () => void;
+  onAddToFavorite: () => void;
+  onAddToPlaylist: () => void;
+}
+
+const MoreMenu: React.FC<MoreMenuProps> = ({
+  visible,
+  onClose,
+  onAddToQueue,
+  onAddToFavorite,
+  onAddToPlaylist,
+}) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <Pressable
+        style={{
+          flex: 1,
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          justifyContent: "flex-end", 
+        }}
+        onPress={onClose}
+      >
+        <View
+          style={{
+            backgroundColor: "#1E1E1E", 
+            padding: 20,
+            width: "100%",
+            maxHeight: "50%", 
+          }}
+        >
+          <View 
+            style={{
+                height: 5, 
+                width: 25,
+                backgroundColor: "white",
+                alignSelf: "center",
+                borderRadius: 3,
+                marginBottom: 15,
+            }} />
+          {/* Tùy chọn Add to Queue */}
+          <View
+            style={{
+                marginTop: 20,
+                marginBottom: 10,
+                flexDirection: "row",
+                alignItems: "center",
+                backgroundColor: "#9D9D9D",
+                paddingVertical: 14,
+                paddingHorizontal: 20,
+                borderBottomWidth: 1,
+                borderBottomColor: "#333",
+                borderRadius: 10,
+            }}
+            >
+            <TouchableOpacity
+                onPress={() => {
+                onAddToQueue();
+                onClose();
+                }}
+                activeOpacity={0.7}
+                style={{
+                flex: 1, 
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between", 
+                }}
+            >
+                <Text style={{ color: APP_COLOR.WHITE, fontSize: 16, fontWeight: "600" }}>
+                Add to Queue
+                </Text>
+
+                <Entypo
+                name="plus"
+                size={20}
+                color={APP_COLOR.WHITE}
+                />
+            </TouchableOpacity>
+            </View>
+
+          {/* Tùy chọn Add to Favorite */}
+          <View
+            style={{
+                marginTop: 20,
+                marginBottom: 0,
+                flexDirection: "row",
+                alignItems: "center",
+                backgroundColor: "#9D9D9D",
+                paddingVertical: 14,
+                paddingHorizontal: 20,
+                borderBottomWidth: 1,
+                borderBottomColor: "#333",
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+            }}
+            >
+            <TouchableOpacity
+                onPress={() => {
+                onAddToFavorite();
+                onClose();
+                }}
+                activeOpacity={0.7}
+                style={{
+                flex: 1, 
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between", 
+                }}
+            >
+                <Text style={{ color: APP_COLOR.WHITE, fontSize: 16, fontWeight: "600" }}>
+                Add to Favorite
+                </Text>
+                <Ionicons name="heart-outline" size={20} color={APP_COLOR.WHITE} />
+            </TouchableOpacity>
+            
+            </View>
+
+          {/* Tùy chọn Add to Playlist */}
+          <View
+            style={{
+                marginBottom: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                backgroundColor: "#9D9D9D",
+                paddingVertical: 14,
+                paddingHorizontal: 20,
+                borderBottomWidth: 1,
+                borderBottomColor: "#333",
+                borderBottomLeftRadius: 10,
+                borderBottomRightRadius: 10,
+            }}
+            >
+            <TouchableOpacity
+                onPress={() => {
+                onAddToPlaylist();
+                onClose();
+                }}
+                activeOpacity={0.7}
+                style={{
+                flex: 1, 
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between", 
+                }}
+            >
+                <Text style={{ color: APP_COLOR.WHITE, fontSize: 16, fontWeight: "600" }}>
+                Add to Playlist
+                </Text>
+                <MaterialIcons name="playlist-add" size={20} color={APP_COLOR.WHITE} />
+            </TouchableOpacity>
+            
+            </View>
+        </View>
+      </Pressable>
+    </Modal>
+  );
+};
+
+export default MoreMenu;
